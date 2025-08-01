225ad4086ca5cc11be5f4a374270e21c3ead1302
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-08-01 16:12:48 +0200

    Add WorkEntry class with validation attributes
    Introduces the `WorkEntry` class in the `WTA_Api.Models` namespace, featuring properties for `WorkEntryId`, `StartDateTime`, `Duration`, `TotalWage`, and `EmployeeId`. Implements data annotations for validation, including range checks for `Duration` and `TotalWage`, and establishes a foreign key relationship with the `Employee` class.

---
+++ WTA-Api/Models/WorkEntry.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WTA_Api.Models
+{
+    public class WorkEntry
+    {
+        [Key]
+        public int WorkEntryId { get; set; }
+
+        [Required]
+        public DateTime StartDateTime { get; set; }
+
+        [Required]
+        [Range(typeof(TimeSpan), "00:00:00", "23:59:59", ErrorMessage = "Duration must be less than 24 hours.")]
+        public TimeSpan Duration { get; set; }
+
+        [Range(0, 24000, ErrorMessage = "Total wage must be between 0 and 24000.")]
+        [DataType(DataType.Currency)]
+        public decimal TotalWage { get; set; }
+
+        [Required]
+        public int EmployeeId { get; set; }
+
+        [ForeignKey(nameof(EmployeeId))]
+        public Employee Employee { get; set; } = null!;
+    }
+}
