b0afa35c843716b17f68452c6bf79c6c9a3f8b2b
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-08-02 15:33:13 +0200

    Added unimplemented WorkEntryController for api endpoint definitions

---
+++ WTA-Api/Controllers/WorkEntryController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WTA_Api.Models;
+
+namespace WTA_Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class WorkEntryController : Controller
+    {
+        [HttpPost]
+        [Route("RegisterWorkEntry")]
+        public async Task<ActionResult<WorkEntry>> AddWorkEntry(WorkEntry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("DeleteWorkEntry")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<WorkEntry>> DeleteWorkEntry(int entryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("UpdateWorkEntry")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<WorkEntry>> UpdateWorkEntry(WorkEntry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("GetWorkEntries")]
+        public async Task<ActionResult<List<WorkEntry>>> GetWorkEntries(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
