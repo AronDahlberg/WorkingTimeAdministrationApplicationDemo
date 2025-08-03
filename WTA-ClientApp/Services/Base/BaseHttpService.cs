8cd0f0f0bff85b2cba33ed378d1421fdba33a905
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-08-03 23:06:31 +0200

    Added BaseHttpService

---
+++ WTA-ClientApp/Services/Base/BaseHttpService.cs
@@ -0,0 +1,20 @@
+﻿using Blazored.LocalStorage;
+
+namespace WTA_ClientApp.Services.Base
+{
+    public class BaseHttpService(ILocalStorageService localStorage, IClient client)
+    {
+        private readonly ILocalStorageService localStorage = localStorage;
+        private readonly IClient client = client;
+
+        protected async Task GetBearerToken()
+        {
+            var token = await localStorage.GetItemAsync<string>("accessToken");
+            if (token != null)
+            {
+                client.HttpClient.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", token);
+            }
+        }
+    }
+}
