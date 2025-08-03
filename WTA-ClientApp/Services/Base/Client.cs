f4738cb50b5cceabad7f727c81b9b06eb9f27079
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2025-08-03 22:04:48 +0200

    Added Blazor WebAssembly client application and configured it to interact with the API and implemented registration

---
+++ WTA-ClientApp/Services/Base/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace WTA_ClientApp.Services.Base
+{
+    public partial class Client : IClient
+    {
+        public HttpClient HttpClient
+        {
+            get
+            {
+                return _httpClient;
+            }
+        }
+    }
+}
